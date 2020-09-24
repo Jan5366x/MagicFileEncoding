13103ed374d5bcb72b0d36043ab10817efb9dad7
Jan Schwien <jan5366@gmail.com>
2020-09-24 20:53:44 +0200

    Add Latin1; Adjust Encoding Logic

---
+++ MagicFileEncoding/EncodingSet/EncodingSetLatin1.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+namespace MagicFileEncoding.EncodingSet
+{
+    public class EncodingSetLatin1 : EncodingSet
+    {
+        public override Encoding GetEncoding()
+        {
+            return Encoding.GetEncoding("iso-8859-1");
+        }
+    }
+}
--- MagicFileEncoding/MagicFileEncoding.cs
+++ MagicFileEncoding/MagicFileEncoding.cs
@@ -39,20 +39,9 @@ namespace MagicFileEncoding
             if (encodingByBom != null)
                 return encodingByBom;
 
-            // BOM not found :(, so try to parse characters into several encodings
-            var encodingByParsingUtf8 = GetEncodingByParsing(filename, Encoding.UTF8);
-            if (encodingByParsingUtf8 != null)
-                return encodingByParsingUtf8;
-
-            var encodingByParsingLatin1 = GetEncodingByParsing(filename, Encoding.GetEncoding("iso-8859-1"));
-            if (encodingByParsingLatin1 != null)
-                return encodingByParsingLatin1;
-
-            var encodingByParsingUTF7 = GetEncodingByParsing(filename, Encoding.UTF7);
-            if (encodingByParsingUTF7 != null)
-                return encodingByParsingUTF7;
-
-            return null;   // no encoding found
+            return (from encodingSet in encodingSets 
+                where encodingSet.Match(filename)
+                select encodingSet.GetEncoding()).FirstOrDefault();
         }
 
 
@@ -77,8 +66,7 @@ namespace MagicFileEncoding
             if (bom[0] == 0xff && bom[1] == 0xfe) return Encoding.Unicode; //UTF-16LE
             if (bom[0] == 0xfe && bom[1] == 0xff) return Encoding.BigEndianUnicode; //UTF-16BE
             if (bom[0] == 0 && bom[1] == 0 && bom[2] == 0xfe && bom[3] == 0xff) return new UTF32Encoding(true, true);  //UTF-32BE
-
-          
+            
             // We actually have no idea what the encoding is if we reach this point, so return default
             return defaultEncoding;
         }
@@ -101,7 +89,4 @@ namespace MagicFileEncoding
             }
         }
     }
-
-
-  
 }
