333587d41a5efcb37a7d681a36074f9c6c789494
Jan Schwien <jan5366@gmail.com>
2020-09-28 00:46:01 +0200

    Adjust testcases to os specific directory separator

--- UnitTests/MagicFileEncodingTest.cs
+++ UnitTests/MagicFileEncodingTest.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Text;
 using NUnit.Framework;
 
@@ -13,16 +14,18 @@ namespace UnitTests
             _mfe = new MagicFileEncoding.MagicFileEncoding();
         }
         
-        [TestCase("\\TestFiles\\A_ANSI.txt")]
-        [TestCase("\\TestFiles\\A_UTF-8.txt")]
-        [TestCase("\\TestFiles\\A_UTF-8-BOM.txt")]
-        [TestCase("\\TestFiles\\A_ISO-8859-1.txt")]
-        [TestCase("\\TestFiles\\A_Windows-1252.txt")]
-        [TestCase("\\TestFiles\\A_UTF-16-LE-BOM.txt")]
-        [TestCase("\\TestFiles\\A_UTF-16-BE-BOM.txt")]
+        [TestCase("/TestFiles/A_ANSI.txt")]
+        [TestCase("/TestFiles/A_UTF-8.txt")]
+        [TestCase("/TestFiles/A_UTF-8-BOM.txt")]
+        [TestCase("/TestFiles/A_ISO-8859-1.txt")]
+        [TestCase("/TestFiles/A_Windows-1252.txt")]
+        [TestCase("/TestFiles/A_UTF-16-LE-BOM.txt")]
+        [TestCase("/TestFiles/A_UTF-16-BE-BOM.txt")]
         public void AutomaticReadAllText(string subFilePath)
         {
-            var filePath = TestContext.CurrentContext.WorkDirectory + subFilePath;
+            var filePath = TestContext.CurrentContext.WorkDirectory + subFilePath
+                .Replace('/', Path.DirectorySeparatorChar);
+            
             Assert.AreEqual("Kleiner Test äöüÄÖÜ?ß", _mfe.AutomaticReadAllText(filePath, Encoding.Unicode));
         }
     }
