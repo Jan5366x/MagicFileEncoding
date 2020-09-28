e459cf7e1b8f86abe57190d3028bb7608ee06b4f
Jan Schwien <jan5366@gmail.com>
2020-09-28 21:13:19 +0200

    Move test files

--- UnitTests/MagicFileEncodingTest.cs
+++ UnitTests/MagicFileEncodingTest.cs
@@ -9,21 +9,21 @@ namespace UnitTests
 {
     public class Tests
     {
-        private MagicFileEncoding.FileEncoding _mfe;
+        private FileEncoding _mfe;
         
         [SetUp]
         public void Setup()
         {
-            _mfe = new MagicFileEncoding.FileEncoding();
+            _mfe = new FileEncoding();
         }
         
-        [TestCase("/TestFiles/A_ANSI.txt")]
-        [TestCase("/TestFiles/A_UTF-8.txt")]
-        [TestCase("/TestFiles/A_UTF-8-BOM.txt")]
-        [TestCase("/TestFiles/A_ISO-8859-1.txt")]
-        [TestCase("/TestFiles/A_Windows-1252.txt")]
-        [TestCase("/TestFiles/A_UTF-16-LE-BOM.txt")]
-        [TestCase("/TestFiles/A_UTF-16-BE-BOM.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_ANSI.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_UTF-8.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_UTF-8-BOM.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_ISO-8859-1.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_Windows-1252.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_UTF-16-LE-BOM.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_UTF-16-BE-BOM.txt")]
         public void AutomaticReadAllText(string subFilePath)
         {
             var filePath = TestContext.CurrentContext.WorkDirectory + subFilePath
@@ -32,13 +32,13 @@ namespace UnitTests
             Assert.AreEqual("Kleiner Test äöüÄÖÜ?ß", _mfe.AutomaticReadAllText(filePath, Encoding.Unicode));
         }
 
-        [TestCase("/TestFiles/A_ANSI.txt")]
-        [TestCase("/TestFiles/A_UTF-8.txt")]
-        [TestCase("/TestFiles/A_UTF-8-BOM.txt")]
-        [TestCase("/TestFiles/A_ISO-8859-1.txt")]
-        [TestCase("/TestFiles/A_Windows-1252.txt")]
-        [TestCase("/TestFiles/A_UTF-16-LE-BOM.txt")]
-        [TestCase("/TestFiles/A_UTF-16-BE-BOM.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_ANSI.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_UTF-8.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_UTF-8-BOM.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_ISO-8859-1.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_Windows-1252.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_UTF-16-LE-BOM.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_UTF-16-BE-BOM.txt")]
         public void LoadAsUnicodeSaveToUtf8(string subFilePath)
         {
             var filePath = TestContext.CurrentContext.WorkDirectory + subFilePath
@@ -50,19 +50,19 @@ namespace UnitTests
             
             _mfe.WriteAllText(tmpFile.Path, text, Encoding.UTF8);
             
-            var expectedResultPath = TestContext.CurrentContext.WorkDirectory + "/TestFiles/A_UTF-8-BOM.txt"
+            var expectedResultPath = TestContext.CurrentContext.WorkDirectory + "/TestFiles/SimpleFiles/A_UTF-8-BOM.txt"
                 .Replace('/', Path.DirectorySeparatorChar);
             
             Assert.IsTrue(IOTools.FileCompare(expectedResultPath, tmpFile.Path));
         }
         
-        [TestCase("/TestFiles/A_ANSI.txt")]
-        [TestCase("/TestFiles/A_UTF-8.txt")]
-        [TestCase("/TestFiles/A_UTF-8-BOM.txt")]
-        [TestCase("/TestFiles/A_ISO-8859-1.txt")]
-        [TestCase("/TestFiles/A_Windows-1252.txt")]
-        [TestCase("/TestFiles/A_UTF-16-LE-BOM.txt")]
-        [TestCase("/TestFiles/A_UTF-16-BE-BOM.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_ANSI.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_UTF-8.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_UTF-8-BOM.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_ISO-8859-1.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_Windows-1252.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_UTF-16-LE-BOM.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_UTF-16-BE-BOM.txt")]
         public void LoadAsUtf8SaveToUtf8(string subFilePath)
         {
             var filePath = TestContext.CurrentContext.WorkDirectory + subFilePath
@@ -74,19 +74,19 @@ namespace UnitTests
             
             _mfe.WriteAllText(tmpFile.Path, text, Encoding.UTF8);
             
-            var expectedResultPath = TestContext.CurrentContext.WorkDirectory + "/TestFiles/A_UTF-8-BOM.txt"
+            var expectedResultPath = TestContext.CurrentContext.WorkDirectory + "/TestFiles/SimpleFiles/A_UTF-8-BOM.txt"
                 .Replace('/', Path.DirectorySeparatorChar);
             
             Assert.IsTrue(IOTools.FileCompare(expectedResultPath, tmpFile.Path));
         }
         
-        [TestCase("/TestFiles/A_ANSI.txt")]
-        [TestCase("/TestFiles/A_UTF-8.txt")]
-        [TestCase("/TestFiles/A_UTF-8-BOM.txt")]
-        [TestCase("/TestFiles/A_ISO-8859-1.txt")]
-        [TestCase("/TestFiles/A_Windows-1252.txt")]
-        [TestCase("/TestFiles/A_UTF-16-LE-BOM.txt")]
-        [TestCase("/TestFiles/A_UTF-16-BE-BOM.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_ANSI.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_UTF-8.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_UTF-8-BOM.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_ISO-8859-1.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_Windows-1252.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_UTF-16-LE-BOM.txt")]
+        [TestCase("/TestFiles/SimpleFiles/A_UTF-16-BE-BOM.txt")]
         public void LoadAsUnicodeSaveToISO_8859_1(string subFilePath)
         {
             var filePath = TestContext.CurrentContext.WorkDirectory + subFilePath
@@ -98,7 +98,7 @@ namespace UnitTests
             
             _mfe.WriteAllText(tmpFile.Path, text, AdditionalEncoding.ISO_8859_1);
             
-            var expectedResultPath = TestContext.CurrentContext.WorkDirectory + "/TestFiles/A_ISO-8859-1.txt"
+            var expectedResultPath = TestContext.CurrentContext.WorkDirectory + "/TestFiles/SimpleFiles/A_ISO-8859-1.txt"
                 .Replace('/', Path.DirectorySeparatorChar);
             
             Assert.IsTrue(IOTools.FileCompare(expectedResultPath, tmpFile.Path));
