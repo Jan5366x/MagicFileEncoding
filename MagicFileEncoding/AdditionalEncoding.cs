62f5640a60852093f3d497138f12acb50c825d0b
Jan Schwien <jan5366@gmail.com>
2021-02-12 20:13:11 +0100

    Add encoding soft fetch

--- MagicFileEncoding/AdditionalEncoding.cs
+++ MagicFileEncoding/AdditionalEncoding.cs
@@ -3,6 +3,10 @@ using System.Text;
 
 namespace MagicFileEncoding
 {  
+    /// <summary>
+    /// <para>List of additional encodings</para>
+    /// Encoding will be <i>null</i> if required codepage can't be retrieved
+    /// </summary>
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     public static class AdditionalEncoding
     {
@@ -13,33 +17,33 @@ namespace MagicFileEncoding
         /// Icelandic, German, Italian, Norwegian, Portuguese, Spanish and Swedish. Only single characters like the
         /// Dutch "ij" or the German quotation marks below are missing.
         /// </summary>
-        public static readonly Encoding ISO_8859_1 = Encoding.GetEncoding("iso-8859-1");
+        public static readonly Encoding ISO_8859_1 = SoftFetchEncoding("iso-8859-1");
         
         /// <summary>
         /// <para>(Latin-2)</para>
         /// This character set contains the script-specific characters for most Central European and Slavic languages.
         /// The character set covers Croatian, Polish, Romanian, Slovak, Slovenian, Czech and Hungarian.
         /// </summary>
-        public static readonly Encoding ISO_8859_2 = Encoding.GetEncoding("iso-8859-2");
+        public static readonly Encoding ISO_8859_2 = SoftFetchEncoding("iso-8859-2");
         
         /// <summary>
         /// <para>(Latin-3)</para>
         /// This character set covers the languages Esperanto, Galician, Maltese and Turkish.
         /// </summary>
-        public static readonly Encoding ISO_8859_3 = Encoding.GetEncoding("iso-8859-3");
+        public static readonly Encoding ISO_8859_3 = SoftFetchEncoding("iso-8859-3");
         
         /// <summary>
         /// <para>(Latin-4)</para>
         /// This character set contains some characters of Estonian, Latvian and Lithuanian languages.
         /// Compare this character set also with ISO 8859-10, which is very similar.
         /// </summary>
-        public static readonly Encoding ISO_8859_4 = Encoding.GetEncoding("iso-8859-4");
+        public static readonly Encoding ISO_8859_4 = SoftFetchEncoding("iso-8859-4");
         
         /// <summary>
         /// This character set contains Cyrillic characters.
         /// It largely covers the Bulgarian, Macedonian, Russian, Serbian and Ukrainian languages.
         /// </summary>
-        public static readonly Encoding ISO_8859_5 = Encoding.GetEncoding("iso-8859-5");
+        public static readonly Encoding ISO_8859_5 = SoftFetchEncoding("iso-8859-5");
         
         /// <summary>
         /// This character set contains characters of Arabic script. However, the representation of the characters
@@ -47,32 +51,32 @@ namespace MagicFileEncoding
         /// they are at the beginning, middle, or end of a word, or individually.
         /// Arabic is further characterized by the fact that the direction of writing is from right to left.
         /// </summary>
-        public static readonly Encoding ISO_8859_6 = Encoding.GetEncoding("iso-8859-6");
+        public static readonly Encoding ISO_8859_6 = SoftFetchEncoding("iso-8859-6");
         
         /// <summary>
         /// This character set contains the characters of the Modern Greek script.
         /// </summary>
-        public static readonly Encoding ISO_8859_7 = Encoding.GetEncoding("iso-8859-7");
+        public static readonly Encoding ISO_8859_7 = SoftFetchEncoding("iso-8859-7");
         
         /// <summary>
         /// This character set contains the characters of the Hebrew script.
         /// As with the Arabic script, the direction of writing is from right to left.
         /// </summary>
-        public static readonly Encoding ISO_8859_8 = Encoding.GetEncoding("iso-8859-8");
+        public static readonly Encoding ISO_8859_8 = SoftFetchEncoding("iso-8859-8");
         
         /// <summary>
         /// <para>(Latin-5)</para>
         /// This character set is specially designed for Turkish. It is based on ISO 8859-1,
         /// but contains Turkish characters instead of the Icelandic special characters.
         /// </summary>
-        public static readonly Encoding ISO_8859_9 = Encoding.GetEncoding("iso-8859-9");
+        public static readonly Encoding ISO_8859_9 = SoftFetchEncoding("iso-8859-9");
         
         /// <summary>
         /// <para>(Latin-6)</para>
         /// This character set specifically contains characters for
         /// the Greenlandic (Inuit) and Lappish (Sami) languages.
         /// </summary>
-        public static readonly Encoding ISO_8859_10 = Encoding.GetEncoding("iso-8859-10");
+        public static readonly Encoding ISO_8859_10 = SoftFetchEncoding("iso-8859-10");
         
         /// <summary>
         /// UTF-32 always encodes a character in exactly 32 bits and is thus the simplest, since no variable character
@@ -82,6 +86,23 @@ namespace MagicFileEncoding
         /// significant byte or the most significant byte is transmitted first,
         /// one speaks of Little Endian (UTF-32LE) or <b>Big Endian (UTF-32BE)</b>. 
         /// </summary>
-        public static readonly Encoding UTF32BE = Encoding.GetEncoding("utf-32BE");
+        public static readonly Encoding UTF32BE = SoftFetchEncoding("utf-32BE");
+
+        /// <summary>
+        /// Get the requested encoding and consume exception if it can't be found in code pages 
+        /// </summary>
+        /// <param name="encoding">The encoding name</param>
+        /// <returns>The encoding object or <i>null</i></returns>
+        private static Encoding SoftFetchEncoding(string encoding)
+        {
+            try
+            {
+               return Encoding.GetEncoding(encoding);
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
